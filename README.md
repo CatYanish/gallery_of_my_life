92a6998c15faa67d29d62d05217839d1420b15b6
Cat Yanish <catyanish@gmail.com>
2017-07-15 17:29:24 -0500

    added photos and show/hide description functionality

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # gallery_of_my_life
+
+TODOS
+
+
+[X] Set up server
+[X] Get angular running
+[X] Show six photos on the DOM.
+
+[X]When someone clicks on a picture, they should see a brief synopsis of why you chose to display that photo.
+
+
+[] Users can also heart/thumbs up/favorite your photo as many times as they want; you'll need to keep track of how many times it was clicked.
+
+[] When you're done, host this on Heroku!
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,41 @@
 console.log('js sourced');
+
+var myApp = angular.module('myApp', []);
+
+
+myApp.controller('GalleryController', function() {
+  var gallery = this;
+
+  gallery.pictures = [
+    { url : "images/MountainView.jpg", visible: true, description: "this is a photo"},
+    { url : "images/Korea.jpg", visible: true, description: "a photo"},
+    { url : "images/MountainView.jpg", visible: true, description: "a photo"},
+    { url : "images/MountainView.jpg", visible: true, description: "a photo"},
+    { url : "images/MountainView.jpg", visible: true, description: "a photo"},
+    { url : "images/MountainView.jpg", visible: true, description: "a photo"},
+  ]; //
+
+
+  gallery.description = [
+    {description: "this is a photo"},
+    {description: "this is a photo"},
+    {description: "this is a photo"},
+    {description: "this is a photo"},
+    {description: "this is a photo"},
+    {description: "this is a photo"},
+  ];
+
+  gallery.description = function(picture) {
+    description.visible = !descrition.visible;
+  };
+
+  gallery.toggle = function(picture) {
+    console.log(picture);
+    picture.visible = !picture.visible;
+  };
+
+
+
+
+
+}); //end of controller
