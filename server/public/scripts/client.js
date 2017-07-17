9b57c5d6f25befb117bf36e7d836d977bc1f4ae4
Cat Yanish <catyanish@Cats-MacBook-Air.local>
2017-07-17 00:05:52 -0500

    finished styling

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -7,12 +7,13 @@ myApp.controller('GalleryController', function() {
   var gallery = this;
 
   gallery.pictures = [
-    { url : "images/MountainView.jpg", visible: true, description: "this is a photo", likes: 0 },
-    { url : "images/Korea.jpg", visible: true, description: "a photo", likes: 0 },
-    { url : "images/MountainView.jpg", visible: true, description: "a photo", likes: 0},
-    { url : "images/MountainView.jpg", visible: true, description: "a photo", likes: 0},
-    { url : "images/MountainView.jpg", visible: true, description: "a photo", likes: 0},
-    { url : "images/MountainView.jpg", visible: true, description: "a photo", likes: 0 }
+    { url : "images/WithThePadres.png", visible: true, description: "I'm really grateful for my parents! They've supported me in all my endeavors for my whole life, and we have a lot of happy memories together.", likes: 0 },
+    { url : "images/MeandPabs.png", visible: true, description: "It's always good to have someone to be weird and have fun with. This is me and Pablo having fun and being weird ", likes: 0 },
+    { url : "images/mountainclimbers.jpg", visible: true, description: "Climb the mountain because it's there! Here, my friend and I struck a pose in the middle of a 17 hour hike! We are intense!", likes: 0},
+    { url : "images/RainbowCode.png", visible: true, description: "NERD ALERT: I like to play with programming languages to make things like this webpage =]", likes: 0},
+    { url : "images/JanieDog.png", visible: true, description: "Dogs are awesome. Here is my family dog.", likes: 0},
+    { url : "images/Korea.jpg", visible: true, description: "Teaching English for a year in South Korea was one of the best life decisions I have ever made. Look at these cuties! White belt at English, but black belt at awesome.", likes: 0 },
+
   ]; //
 
 
